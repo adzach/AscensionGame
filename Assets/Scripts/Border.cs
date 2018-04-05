2e9e6eff66d3a250d6136abd5cf62200eb8a374f
RJ <ballerarge@gmail.com>
2018-04-05 15:50:36 -0400

    Added in intial scene stuff

---
+++ Assets/Scripts/Border.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Border : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		GameObject go = coll.gameObject;
+		switch (go.tag) {
+		case "Hero":
+			// Handle collision with hero
+			break;
+		case "Enemy":
+			// Handle collision with enemy
+			break;
+
+		default:
+			print ("Something collided with a border. It was: " + go.name);
+			break;
+		}
+	}
+
+}
