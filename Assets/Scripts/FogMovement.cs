6c6c6ad20d365c147c20ae8bebcf6ed71adfb5fd
RJ <ballerarge@gmail.com>
2018-04-19 15:48:31 -0400

    Removed unused code from fog script

--- Assets/Scripts/FogMovement.cs
+++ Assets/Scripts/FogMovement.cs
@@ -7,51 +7,16 @@ public class FogMovement : MonoBehaviour {
 	[Header ("Set in Inspector")]
 	public GameObject level;
 	public float velocity;
-//	public int maxFlow;
-
-//	private float widthLevel;
-//	private float widthFog;
-//	private float leftEdgeLevel;
-//	private float rightEdgeLevel;
-//	private int frameCount;
-//	private bool flowRight;
 	private Vector3 startPosition;
 
 	// Use this for initialization
 	void Start () {
 		transform.position = new Vector3 (0, 0, -1);
-//		widthLevel = level.GetComponent<Renderer> ().bounds.size.x;
-//		widthFog = transform.gameObject.GetComponent<Renderer> ().bounds.size.x;
-//		leftEdgeLevel = level.transform.position.x - (widthLevel / 2);
-//		rightEdgeLevel = level.transform.position.x + (widthLevel / 2);
-//		frameCount = 0;
-//		flowRight = true;
 		startPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		float leftEdgeFog = transform.position.x - (widthFog / 2);
-//		float rightEdgeFog = transform.position.x + (widthFog / 2);
-//
-//		if (leftEdgeFog >= leftEdgeLevel) {
-//			transform.position = new Vector3 (rightEdgeLevel - (widthLevel / 2), transform.position.y, transform.position.z);
-//		}
-//		transform.position = new Vector3 (transform.position.x + velocity, transform.position.y, transform.position.z);
-
-//		if (flowRight) {
-//			transform.position = new Vector3 (transform.position.x + velocity, transform.position.y, transform.position.z);
-//		} else {
-//			transform.position = new Vector3 (transform.position.x - velocity, transform.position.y, transform.position.z);
-//		}
-//
-//		if (frameCount >= maxFlow) {
-//			flowRight = !flowRight;
-//			frameCount = -1;
-//		}
-//
-//		frameCount++;
-
 		transform.position = startPosition + new Vector3 (Mathf.Sin (Time.time) * velocity, 0.0f, 0.0f);
 	}
 }
