e2143828441da8fdea6c6bee3d599d0b1d74f3d8
michael nutt <nuttmj@nuttmj-5520.rose-hulman.edu>
2018-04-26 14:29:56 -0400

    Character movement and ability

---
+++ Assets/abilityEngine.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abilityEngine : MonoBehaviour {
+    public enum Character {Ooga, otherchar1, otherchar2};
+    public Character curchar = Character.Ooga;
+    public GameObject fireAbility;
+    public GameObject abilityHolder;
+    public bool canAbility1 = true;
+    public countdownSM countMan;
+    public int cooldownVal = 10;
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("1")&&canAbility1) ability1();
+        if (Input.GetKeyDown("2")) ability2();
+        if (Input.GetKeyDown("3")) ability3();
+    }
+
+    public void ability1()
+    {
+        if(curchar==Character.Ooga)
+        {
+            GameObject tempabl = Instantiate(fireAbility,this.transform.position,this.transform.rotation,abilityHolder.transform);
+            tempabl.SendMessage("ability1");
+        }
+        canAbility1 = false;
+        countMan.countdown(this.gameObject,cooldownVal,"1");
+    }
+
+    public void ability2()
+    {
+
+    }
+
+    public void ability3()
+    {
+
+    }
+
+    public void finishedCount1()
+    {
+        canAbility1 = true;
+    }
+}
---
+++ Assets/abilityInst.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abilityInst : MonoBehaviour
+{
+    public enum Character { Ooga, otherchar1, otherchar2 };
+    public Character curchar = Character.Ooga;
+    protected Animator anim;
+    public Animation fireanim;
+    public bool running = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running&&(!anim.GetCurrentAnimatorStateInfo(0).IsName("fireAbility")))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("fireAbility") && other.gameObject.layer == LayerMask.NameToLayer("Enemies"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    public void ability1()
+    {
+        anim = GetComponent<Animator>();
+        if (curchar == Character.Ooga)
+        {
+            anim.Play("fireAbility");
+        }
+
+        running = true;
+    }
+
+    public void ability2()
+    {
+
+    }
+
+    public void ability3()
+    {
+
+    }
+}
